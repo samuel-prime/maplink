139cdc622561a9ce6fd581eb5988df0be1980a34
samuel-prime <samuel.lucas@primecargo.com.br>
2024-11-07 08:07:06 -0300

    refactor(geocode): compress everything into a search method

--- modules/geocode/index.ts
+++ modules/geocode/index.ts
@@ -1,108 +1,52 @@
 import { MaplinkModule } from "core/maplink/module";
-import type { MaplinkApi, Module } from "core/maplink/types";
+import type { ModuleScope } from "core/maplink/scope";
+import type { _SDK } from "core/maplink/types";
 import assert from "node:assert";
-import type { Either, Failure, FilterKeys } from "utils/types";
 import type { _Geocode } from "./types";
 
-/**
- * ### Geocode API
- *
- * The Geocode Autocomplete API is a Restful API capable of returning coordinates
- * (latitude and longitude) from an address or vice versa.
- *
- * With the Geocode Autocomplete API you can:
- *
- * - Obtain coordinates and geographic data from an address, or part of it;
- * - Obtain information about an address from its coordinates;
- * - Autocomplete and normalize addresses with suggestions based on user input;
- * - Filter results based on specific parameters, such as location type, country, etc.
- *
- * For more information about the Geocode API, see the [official documentation](https://developers.maplink.global/introducao-geocode/).
- */
 export class Geocode extends MaplinkModule {
+  static readonly METADATA: _SDK.Module.Metadata = {
+    name: "geocode",
+    version: "0.0.1",
+    description: "Handles geocoding operations.",
+  };
+
   static readonly #ENDPOINT = "/geocode/v1";
 
-  constructor(scope: Module.Scope) {
-    super(scope, {
-      name: "geocode",
-      version: "1.0.0",
-      description: "Handles geocoding operations.",
-    });
+  constructor(scope: ModuleScope) {
+    super(scope, Geocode.METADATA);
 
     this.api.baseUrl.endpoint = Geocode.#ENDPOINT;
     this.logger.prefix = "[GEOCODE]";
     this.globalSearch = false;
   }
 
-  // Public Methods --------------------------------------------------------------------------------
+  async search<T extends _Geocode.Search.Input, U = _Geocode.Search.SelectOutput<T>>(input: T) {
+    const endpoint = this.#resolveEndpointByInput(input);
 
-  /**
-   * Search for addresses based on partial address information. The search can be performed using:
-   *
-   * - A `string`, in URL query format. It uses the `suggestion` mode;
-   * - An `object` in the `SearchAddress` interface. It uses the `default` mode;
-   * - An `array` of `SearchAddress` objects. It uses the `multiDefault` mode.
-   *
-   * @remarks The `multiDefault` mode requires to pass an `id` property in each object, which
-   * will be used to identify the results.
-   */
-  async search<T extends _Geocode.ModeInput<"suggestion" | "default" | "multiDefault">>(input: T) {
     return typeof input === "string"
-      ? this.#searchByParams("/suggestions", input)
-      : this.#searchByBody(Array.isArray(input) ? "/multi-geocode" : "/geocode", input);
+      ? this.api.get<U, _SDK.Api.DefaultErrorResponse>(endpoint, { params: { q: input } })
+      : this.api.post<U, _SDK.Api.DefaultErrorResponse>(endpoint, input);
   }
 
-  /**
-   * Search for addresses based on given coordinates.
-   * Depending on the number of coordinates, the search will be performed using:
-   *
-   * - For 1 coordinate, an `object` in the `Coordinates` interface. It uses the `reverse` mode;
-   * - For multiple coordinates, an `array` of `Coordinates` objects. It uses the `multiReverse` mode.
-   *
-   * @remarks
-   * The `multiReverse` mode requires to pass an `id` property in each object, which will be used to identify the results.
-   * This mode also returns the `extended` version of the `Address` interface.
-   */
-  async reverseSearch<T extends _Geocode.ModeInput<"reverse" | "multiReverse">>(input: T) {
-    return this.#searchByBody("/reverse", input);
+  #resolveEndpointByInput(input: _Geocode.Search.Input) {
+    if (typeof input === "string") return "/suggestions";
+
+    if (Array.isArray(input)) {
+      assert(input.length, "The search array must not be empty.");
+      const [sample] = input;
+
+      const isCoord = Object.hasOwn(sample, "lat") && Object.hasOwn(sample, "lon");
+      if (isCoord) return input.length > 1 ? "/multi-reverse" : "/reverse";
+
+      return "/multi-geocode";
+    }
+
+    return "/geocode";
   }
 
-  // Getters and Setters ---------------------------------------------------------------------------
-
-  /**
-   * Enables or disables the global search.
-   * When enabled, the search can be performed outside of Brazil.
-   */
   set globalSearch(value: boolean) {
-    assert(typeof value === "boolean", "The global search must be a boolean.");
-    this.api.params = ["globalSearch", value];
+    this.api.defaults.params = { globalSearch: !!value };
     this.logger.info(`Global search is ${value ? "enabled" : "disabled"}.`);
-  }
-
-  // Private Methods -------------------------------------------------------------------------------
-
-  async #searchByBody<
-    T extends _Geocode.ModeInput<FilterKeys<_Geocode.SearchModes, [object, any]>>,
-    U = _Geocode.SelectModeOutput<T>,
-  >(endpoint: string, input: T): Promise<Either<U, Error>> {
-    const { kind, value } = await this.api.post<U, MaplinkApi.DefaultErrorResponse>(
-      endpoint,
-      input,
-    );
-    return kind === "failure" ? this.#handleError(value) : { kind: "success", value };
-  }
-
-  async #searchByParams<
-    T extends _Geocode.ModeInput<FilterKeys<_Geocode.SearchModes, [string, any]>>,
-    U = _Geocode.SelectModeOutput<T>,
-  >(endpoint: string, input: T): Promise<Either<U, Error>> {
-    const { kind, value } = await this.api.get<U, MaplinkApi.DefaultErrorResponse>(endpoint, {
-      params: { q: input },
-    });
-    return kind === "failure" ? this.#handleError(value) : { kind: "success", value };
-  }
-
-  #handleError(value: Error | MaplinkApi.DefaultErrorResponse): Failure<Error> {
-    return { kind: "failure", value: value instanceof Error ? value : new Error(value.title) };
   }
 }
--- modules/geocode/types.ts
+++ modules/geocode/types.ts
@@ -1,189 +1,80 @@
-import type { NonEmptyArray, NonEmptyObject } from "utils/types";
+import type { FilterKeys, NonEmptyArray, NonEmptyObject } from "utils/types";
 
 export namespace _Geocode {
-  // Geocode Generic Types -------------------------------------------------------------------------
-
-  /**
-   * The type of the geocode search.
-   * Represents the approach used to find the address(es).
-   */
-  export type SearchApproaches = "ZIPCODE" | "STATE" | "CITY" | "DISTRICT";
-
-  /**
-   * The search modes available for the geocode module.
-   * Each mode has an associated search method and response type.
-   */
-  export interface SearchModes {
-    default: [SearchAddress, Address];
-    multiDefault: [NonEmptyArray<{ id: string } & SearchAddress>, Address];
-    suggestion: [string, Address];
-    reverse: [[Coordinates], Address];
-    multiReverse: [NonEmptyArray<{ id: string } & Coordinates>, ExtendedAddress];
-  }
-
-  /**
-   * The available search modes for the geocode module.
-   */
-  export type Modes = keyof SearchModes;
-
-  /**
-   * The input type for each search mode.
-   */
-  export type ModeInput<K extends Modes = Modes> = SearchModes[K][0];
-
-  /**
-   * The output type for each search mode.
-   */
-  export type ModeOutput<K extends Modes = Modes> = Response<SearchModes[K][1]>;
-
-  /**
-   * Selects the output type based on the input type.
-   */
-  export type SelectModeOutput<T extends ModeInput> = T extends ModeInput<"default">
-    ? ModeOutput<"default">
-    : T extends ModeInput<"multiDefault">
-      ? ModeOutput<"multiDefault">
-      : T extends ModeInput<"suggestion">
-        ? ModeOutput<"suggestion">
-        : T extends ModeInput<"reverse">
-          ? ModeOutput<"reverse">
-          : T extends ModeInput<"multiReverse">
-            ? ModeOutput<"multiReverse">
-            : never;
-
-  // Geocode Address Types -------------------------------------------------------------------------
-
-  /**
-   * The coordinates in a geocode address.
-   */
-  export interface Coordinates {
+  export interface Coords {
     readonly lat: number;
     readonly lon: number;
   }
 
-  /**
-   * The state information in a geocode address.
-   */
-  export interface AddressState {
-    readonly code: string;
-    readonly name: string;
+  export namespace Search {
+    export type Approach = "ZIPCODE" | "STATE" | "CITY" | "DISTRICT";
+
+    interface Modes {
+      default: [Address, Response.Address];
+      multiDefault: [NonEmptyArray<{ id: string } & Address>, Response.Address];
+      suggestion: [string, Response.Address];
+      reverse: [[Coords], Response.Address];
+      multiReverse: [NonEmptyArray<{ id: string } & Coords>, Response.ExtendedAddress];
+    }
+
+    export type ModesNames = keyof Modes;
+    export type Input<K extends ModesNames = ModesNames> = Modes[K][0];
+    export type Output<K extends ModesNames = ModesNames> = Response.Data<Modes[K][1]>;
+    export type SelectOutput<T extends Input> = T extends Input<infer U> ? Output<U> : never;
+
+    export type ByBody = Input<FilterKeys<Modes, [object, any]>>;
+    export type ByParams = Input<FilterKeys<Modes, [string, any]>>;
+
+    export type Address = RequiredAddressParams & OptionalAddressParams;
+
+    export type RequiredAddressParams = NonEmptyObject<{
+      road: string;
+      number: number;
+      city: string;
+      state: string;
+      country: string;
+      district: string;
+      zipcode: string;
+    }>;
+
+    export type OptionalAddressParams = Partial<{ type: Approach }>;
   }
 
-  /**
-   * Represents the default geocode address.
-   */
-  export interface Address {
-    readonly road: string;
-    readonly district: string;
-    readonly zipCode: string;
-    readonly city: string;
-    readonly state: AddressState;
-    readonly mainLocation: Coordinates;
+  export namespace Response {
+    export interface Data<T extends Address | ExtendedAddress> {
+      readonly found: number;
+      readonly results: ReadonlyArray<{
+        readonly id: string;
+        readonly address: T;
+        readonly type: Search.Approach;
+        readonly score: number;
+        readonly distance?: number;
+        readonly label: string;
+      }>;
+    }
+
+    export interface State {
+      readonly code: string;
+      readonly name: string;
+    }
+
+    export interface Address {
+      readonly road: string;
+      readonly district: string;
+      readonly zipCode: string;
+      readonly city: string;
+      readonly state: State;
+      readonly mainLocation: Coords;
+    }
+
+    export interface ExtendedAddress extends Address {
+      readonly geometry: [Coords, Coords];
+      readonly leftZipCode: string;
+      readonly rightZipCode: string;
+      readonly leftFirstNumber: number;
+      readonly leftLastNumber: number;
+      readonly rightFirstNumber: number;
+      readonly rightLastNumber: number;
+    }
   }
-
-  /**
-   * Extends Address to include additional properties.
-   * This is used on **multi reverse mode** searches.
-   */
-  export interface ExtendedAddress extends Address {
-    /**
-     * The geometry coordinates.
-     * It represents the approximate area of the address.
-     */
-    readonly geometry: [Coordinates, Coordinates];
-    readonly leftZipCode: string;
-    readonly rightZipCode: string;
-    readonly leftFirstNumber: number;
-    readonly leftLastNumber: number;
-    readonly rightFirstNumber: number;
-    readonly rightLastNumber: number;
-  }
-
-  // Geocode Result Types --------------------------------------------------------------------------
-
-  /**
-   * Represents a single result in a geocode response.
-   */
-  export interface Result<T extends Address | ExtendedAddress> {
-    /**
-     * The unique identifier for the result.
-     * It can be set in the request, otherwise it will be generated as an UUID.
-     */
-    readonly id: string;
-    /**
-     * The address information.
-     * It is `extended` when using the **multi reverse mode** search.
-     */
-    readonly address: T;
-    /**
-     * The type of the approach used by the geocode to search.
-     */
-    readonly type: SearchApproaches;
-    /**
-     * Reference score of the geocoding process result.
-     * The higher the number, the more reliable the result is.
-     */
-    readonly score: number;
-    /**
-     * Distance in meters from the sent coordinate.
-     * It only appears in **multi reverse mode** searches.
-     */
-    readonly distance?: number;
-    /**
-     * Full address found used in the geocoding process.
-     */
-    readonly label: string;
-  }
-
-  /**
-   * The response from a geocode search.
-   */
-  export interface Response<T extends Address | ExtendedAddress> {
-    readonly found: number;
-    readonly results: ReadonlyArray<Result<T>>;
-  }
-
-  // Search Address Types --------------------------------------------------------------------------
-
-  /**
-   * The required parameters for a search address.
-   * Requires at least one of the properties to be set.
-   */
-  export type RequiredSearchAddressParams = NonEmptyObject<{
-    road: string;
-    /**
-     * The street number.
-     * DO NOT include additional address details.
-     */
-    number: number;
-    city: string;
-    /**
-     * The state code.
-     * MUST be in uppercase and abbreviated.
-     */
-    state: string;
-    /**
-     * The country name.
-     * Must set **globalSearch** to true in order to search outside of Brazil.
-     */
-    country: string;
-    district: string;
-    zipcode: string;
-  }>;
-
-  /**
-   * The optional parameters for a search address.
-   */
-  export interface OptionalSearchAddressParams {
-    /**
-     * Limits the type of search.
-     * Only for addresses in Brazil and if the **globalSearch** parameter is not provided.
-     */
-    type?: SearchApproaches;
-  }
-
-  /**
-   * The combined required and optional parameters for a search address.
-   */
-  export type SearchAddress = RequiredSearchAddressParams & OptionalSearchAddressParams;
 }
