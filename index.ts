c1969fe5e717ea525e5825204fa66e774f219dc2
samuel-prime <samuel.lucas@primecargo.com.br>
2024-11-18 16:41:58 -0300

    build: add planning module to export file

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import { MaplinkSDK } from "core/maplink";
 
 export { MaplinkModule } from "core/maplink/module";
 export { Geocode } from "modules/geocode";
+export { Planning } from "modules/planning";
+export { PlanningProblem } from "modules/planning/problem";
 export { MaplinkSDK };
 
 export default MaplinkSDK;
