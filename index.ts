1612d9b37ea08282622008b9b240571f42f00aca
samuel-prime <samuel.lucas@primecargo.com.br>
2024-12-14 23:05:07 -0300

    fix: export trip module

--- index.ts
+++ index.ts
@@ -3,3 +3,5 @@ export { MaplinkModule } from "core/maplink/module";
 export { Geocode } from "modules/geocode";
 export { Planning } from "modules/planning";
 export { PlanningProblem } from "modules/planning/problem";
+export { Trip } from "modules/trip";
+
